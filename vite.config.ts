1a1101fb840c83ae604c44fa769b6f6457fe98d9
Ievgen <eugenegudima@gmail.com>
2025-11-30 10:19:22 +0200

    Change vite confog to add proxy to access rest api

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 // vite.config.ts
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
@@ -8,31 +8,42 @@ import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 import { intlayer } from "vite-intlayer";
 
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    tailwindcss(),
-    nitro(),
-    viteReact(),
-    intlayer(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
-  },
-  build: {
-    cssMinify: "lightningcss",
-    minify: "esbuild",
-  },
-  nitro: {
-    preset: "bun",
-    prerender: {
-      routes: ["/", "/tank", "/table", "/journal"],
+    plugins: [
+      tsConfigPaths(),
+      tanstackStart(),
+      tailwindcss(),
+      nitro(),
+      viteReact(),
+      intlayer(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    build: {
+      cssMinify: "lightningcss",
+      minify: "esbuild",
+    },
+    nitro: {
+      preset: "bun",
+      prerender: {
+        routes: ["/", "/tank", "/table", "/journal"],
+      },
+    },
+  };
 });
