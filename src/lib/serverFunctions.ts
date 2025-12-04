22b8ac87f6f85643be0533814a824d14845efd48
Ievgen Gudyma <ievgen@gudyma.com>
2025-12-04 16:05:19 +0200

    added test server env

--- src/lib/serverFunctions.ts
+++ src/lib/serverFunctions.ts
@@ -1,7 +1,9 @@
 import { createServerFn } from "@tanstack/react-start";
 
+const test = process.env.DB_TEST || "--";
+
 export const getTanks = createServerFn().handler(async () => {
   // This runs only on the server
-  console.log("Hello");
+  console.log(test);
   return new Date().toISOString();
 });
